cb97906767361bf68f720bff8ea7d1e23df43f5f
FeelsGo-od <andrew.sokolowsky@gmail.com>
2022-11-11 17:11:17 +0100

    fix header dropdown

--- modules/Dropdown/hoverLink.js
+++ modules/Dropdown/hoverLink.js
@@ -5,10 +5,12 @@ export default function hoverLink(dropdownBtn) {
   dropdownBtn.addEventListener('mouseover', function () {
     dropdownBlock.style.opacity = 1;
     dropdownBlock.style.zIndex = 99;
+    dropdownBlock.style.pointerEvents = 'all';
   });
 
   dropdownBtn.addEventListener('mouseout', function () {
     dropdownBlock.style.opacity = 0;
     dropdownBlock.style.zIndex = -1;
+    dropdownBlock.style.pointerEvents = 'none';
   });
 }
